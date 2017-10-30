c0d922eef879b18061b25317fa229f4b8c7655a2
Infante123 <juda_invi@hotmail.com>
2017-10-30 11:54:29 -0600

    actualizacion1

--- segundoparcial.cpp
+++ segundoparcial.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
 using namespace std;
-int main(){
-	cout<<"Examen 2do,parcial Infante Vite Juan David";
-	cout<<"Este es la primera modificacion, se le agrego coma y ortografia"
+int main()
+{
+	cout<<"examen 2do parciaol, Juan David Infante Vite"
+    int x;
+    int s;
+    int a;
+
+    cout<< "\n\n\tIngrese el caracter: ";
+    cin>>a;
+    cout<< "\n\n\tIngrese el numero de veces a repetir: ";
+    cin>>s;
+    for (x=1;x<=s;x++)
+    cout<<""<<x<<"";
+    getch();
+system("pause");
 }
